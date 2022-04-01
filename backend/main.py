33089bc2bb5935187baf909c4d0f62a0c9f2a528
Florian <florian.formentini@gmail.com>
2022-04-01 15:57:56 +0200

    try to deploy

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,5 @@
 """https://testdriven.io/blog/fastapi-streamlit/
 """
-import os
 import uvicorn
 import pandas as pd
 import numpy as np
@@ -11,7 +10,6 @@ from typing import List
 
 from data_models import Client, PredictionOut
 
-PORT = os.getenv('PORT') or 8080
 
 app = FastAPI(
     title="OC IML P9 (bonus) - Scoring model API")
